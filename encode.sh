d429fc11a2ea2e8a00d5f5bf041fe6d61a1ab82d
Paul Furley <paul@paulfurley.com>
2013-08-17 12:25:24 +0100

    Replaced ls * with find because wildcard was expanding to too many arguments and failing.

--- encode.sh
+++ encode.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
 TMP_FILE=$(mktemp)
 OUT_FILE=~/Dropbox/ChilliCam/timelapse.avi
 
-ls ~/Desktop/snapshots/*.jpg > ${TMP_FILE}
-nice -19 mencoder -nosound -ovc lavc -lavcopts vcodec=mjpeg -o ${OUT_FILE} -mf type=jpeg:fps=24 mf://@${TMP_FILE}
+find ~/Desktop/snapshots/ -iname '*.jpg' |sort > ${TMP_FILE}
+nice -19 mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4 -o ${OUT_FILE} -mf type=jpeg:fps=24 mf://@${TMP_FILE}
 
 rm ${TMP_FILE}
